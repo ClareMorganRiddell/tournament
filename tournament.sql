59c35c86b01fcf219d52d818ad9fcd3a683eaae0
ClareMorganRiddell <c.morganriddell@gmail.com>
2017-07-07 17:38:56 +0200

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -1,13 +1,4 @@
 
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
-
-
 CREATE TABLE players( player_name TEXT,
 					  player_id SERIAL PRIMARY KEY);
 
